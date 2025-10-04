7b44e1d86bc27ce61b8381b64af0f94661b775b5
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-04 19:13:28 +0200

    gone back to a single print statement

--- main.go
+++ main.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/aminnairi/gofin/budget"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Coming soon, being able to create your budget from your terminal!")
-
-	budget := budget.Budget{
-		Start: time.Date(2025, time.September, 1, 0, 0, 0, 0, time.Local),
-		Incomes: []budget.Income{
-			{
-				// Salaire
-				Amount:          3430,
-				Start:           time.Date(2025, time.September, 1, 0, 0, 0, 0, time.Local),
-				End:             time.Date(2025, time.December, 31, 0, 0, 0, 0, time.Local),
-				MonthOccurrence: 1,
-			},
-		},
-		Expenses: []budget.Expense{},
-	}
-
-	forecast := budget.Forecast(time.Date(2025, time.December, 31, 0, 0, 0, 0, time.Local))
-
-	fmt.Println("Budget forecast:", forecast)
 }
