5ce3edecccb1587e137f192cf9019798dc0c703f
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-04 10:51:53 +0200

    added tests for the credit cost function

--- finance/finance.go
+++ finance/finance.go
@@ -1,5 +1,7 @@
 package finance
 
+import "math"
+
 func GetFinalCapital(year uint8, interestInPercentage float32, monthlyInvestedCapital float32) (capital float32) {
 	for range year {
 		var yearlyInvestedAmount float32 = monthlyInvestedCapital * 12
@@ -9,4 +11,30 @@ func GetFinalCapital(year uint8, interestInPercentage float32, monthlyInvestedCa
 	}
 
 	return capital
+}
+
+func TotalCreditCost(
+	loanAmount float64,
+	durationInMonths int,
+	annualRateInPercentage float64,
+	annualInsuranceFeeInPercentage float64,
+) (cost float64, rate float64) {
+	monthlyRate := annualRateInPercentage / 100 / 12
+	duration := float64(durationInMonths)
+	powerTerm := math.Pow(1+monthlyRate, -duration)
+	denominator := 1 - powerTerm
+
+	if monthlyRate == 0 || denominator == 0 {
+		cost = 0
+	} else {
+		monthlyAnnuity := loanAmount * (monthlyRate / denominator)
+		totalRepaid := monthlyAnnuity * duration
+		cost = totalRepaid - loanAmount
+	}
+
+	insuranceFee := loanAmount * (annualInsuranceFeeInPercentage / 100) * (duration / 12)
+	cost += insuranceFee
+	rate = cost / loanAmount
+
+	return cost, rate
 }
