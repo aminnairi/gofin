2897472e5ef3eeb48a1695ae3b03640b0b983edd
aminnairi <18418459+aminnairi@users.noreply.github.com>
2025-10-04 10:51:38 +0200

    new function for calculating the credit cost

--- finance/finance_test.go
+++ finance/finance_test.go
@@ -1,11 +1,30 @@
 package finance
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestGetCapitalAt(t *testing.T) {
 	capital := GetFinalCapital(38, 10, 500)
 
 	if capital != 2402688.5 {
 		t.Errorf("Capital invested should be 2402688,5, %f computed", capital)
+	}
+}
+
+func TestTotalCreditCost(t *testing.T) {
+	expectedCost := 36886.0
+	expectedRate := 25.0
+	cost, rate := TotalCreditCost(150_000, 20*12, 1.75, 0.3)
+	roundedRate := math.Round(rate * 100)
+	roundedCost := math.Round(cost)
+
+	if roundedCost != expectedCost {
+		t.Errorf("Expected credit cost to be %f, %f received.", expectedCost, roundedCost)
+	}
+
+	if roundedRate != expectedRate {
+		t.Errorf("Expected credit rate to be %f%%, %f%% received.", expectedRate, roundedRate)
 	}
 }
